dac4464924e8c7f243bf69e70111d7f3f0e9b85a
david-shakaya <devidoz666@gmail.com>
2021-01-27 19:02:41 +0200

    fucking genres is done

--- index.js
+++ index.js
@@ -1,67 +1,73 @@
 const add = document.querySelector('.add');
+const listGenres = [
+{ id: 28, name: "Action" },
+{id: 12, name: "Adventure"},
+{id: 16, name: "Animation"},
+{id: 35, name: "Comedy"},
+{id: 80, name: "Crime"},
+{id: 99, name: "Documentary"},
+{id: 18, name: "Drama"},
+{id: 10751, name: "Family"},
+{id: 14, name: "Fantasy"},
+{id: 36, name: "History"},
+{id: 27, name: "Horror"},
+{id: 10402, name: "Music"},
+{id: 9648, name: "Mystery"},
+{id: 10749, name: "Romance"},
+{id: 878, name: "Science Fiction"},
+{id: 10770, name: "TV Movie"},
+{id: 53, name: "Thriller"},
+{id: 10752, name: "War"},
+{id: 37, name: "Western"}]
 
-let genres;
 
-let genreId = [];
 const startingUrl = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
-const pageNumber = 1
-const query = 'usa'
+const pageNumber = 2
+const query = 'p'
 const API = `https://api.themoviedb.org/3/search/movie?api_key=ffddee44025dd24685ea61d637d56d24&language=en-US&query=${query}&page=${pageNumber}&include_adult=false`
 fetch(API)
 .then(res => res.json())
     .then(data => {
-    data.results.forEach(el => {
-        // console.log(el.genre_ids);
-        genreId.push(el.genre_ids)
+       makeNewObjectFilms(data)
+    })   
 
-        add.insertAdjacentHTML('beforeend', `<div class="movie-card">
-        <img src="${startingUrl}${el.poster_path}" alt=""/>
-        <h3>${el.original_title}</h3> 
-        </div>`) 
-        console.log(el.genre_ids);
-    })
-        const s = genreId.find(el => {
-            console.log('el',el);
-           getGenres(el)
-        })
+const makeNewObjectFilms = function (data) {
+  const newData = data.results.map((item) => {
+    let curentGenres = [];
+    item.genre_ids.map((id) => {
+      const found = listGenres.find((item) => item.id === id);
+      curentGenres.push(found.name);
+    });
+    if (curentGenres.length >= 3) {
+      const normalizedGenres = curentGenres.slice(0, 2);
+      normalizedGenres.push("Other");
+      item.genre_ids = normalizedGenres.join(', ')
+      item.release_date = item.release_date.slice(0, 4);
+    } else {
+      item.genre_ids = curentGenres.join(', ');
+      if (item.release_date) item.release_date = item.release_date.slice(0, 4);
+      }
 
-        console.log('s', s);
-       
-        // console.log(ganres);
-})
-
-function getGenres(id) {
-   return  (fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=ffddee44025dd24685ea61d637d56d24')
-        .then(res => res.json())
-       .then(data => {
-//            const text = document.querySelectorAll('.ganre')
-//            text.forEach(el => {
-//                el.textContent = `${data.genres.find(el => {
-//                    if (el.id === genre_ids[0]) {
-//                        return el.name
-//                    }
-//                })}
-// `           })
-           
-           const div =document.querySelectorAll('.movie-card');
-           genres = data.genres
-        //    console.log(genres);
-
-           const p = genres.forEach(el => { 
-              if (el.id === id) {
-                   console.log(el.name);
-                  div.forEach(elem => {
-                       elem.insertAdjacentHTML('beforeend', `<p>${el.name}</p>`)
-                   })
-               }
-           })
-           
+      console.log(item);
+      addMarkup(item)
+    //   return item;
+    //   console.log(newGenres);
+    //   newGenres.forEach(el => console.log(el))
+  });
  
-             
-          
-          /* `<p>${data.genres[0].name}<p/>`  */
-        }))
+};
+
+function addMarkup(item) {
+    return add.insertAdjacentHTML('beforeend', `<div  class="movie-card-wrapper">
+        <div class="movie-card">
+        <img src="${startingUrl}${item.poster_path}" alt="" width="150px"/>
+        <h3>${item.original_title}</h3> 
+        <p class="js-genre" >${item.genre_ids}| ${item.release_date}  ${item.vote_average}</p>
+        </div>
+        </div>`) 
+
 }
 
-//  console.log(arr);
-    // getGenres()
+/* -----Pagination -------- */
+
+
